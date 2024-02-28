06d05154b45c5e2ad8662085120b657069c77f81
albertoferri <falberto99@gmail.com>
2024-02-28 15:08:01 +0100

    esercizio completato -- stampato titolo

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import AppTitle from './components/AppTitle.vue';
+export default{
+  components: {
+    AppTitle
+  }
+}
 </script>
 
 <template>
+
+  <div>
+    <AppTitle></AppTitle>
+  </div>
   
 </template>
 
-<style>
+<style scoped>
+
 </style>
--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -1,11 +1,34 @@
+<!-- --------QUI VA I TUOI SCRIPT JS LEGATI A QUESTO COMPONENTS IN PARTICOLARE-------- -->
 <script>
+
+    // ----------
 
 </script>
 
+<!-- --------QUI VA L'HTML-------- -->
 <template>
-    
+    <div>
+        <h1>LA MIA PRIMA APP CON VITE!</h1>
+    </div>
 </template>
 
-<style>
+
+
+<!-- --------QUI VA IL CSS-------- -->
+<style scoped>
+
+template{
+    display: flex;
+    justify-content: center;
+}
+
+h1{
+    color: rgb(192, 201, 20);
+    margin-top: 100px;
+    padding: 20px 30px;
+    border: 3px solid red;
+    border-radius: 20px;
+    border-color: rgb(192, 201, 20);
+}
 
 </style>
